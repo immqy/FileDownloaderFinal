9a873350d1c84b545fc310bbb02b5411ea2a2646
pengjianbo <pengjianbo@finalteam.cn>
2016-02-15 13:16:16 +0800

    修改日志类

--- filedownloaderfinal/src/main/java/cn/finalteam/filedownloaderfinal/ILogger.java
+++ filedownloaderfinal/src/main/java/cn/finalteam/filedownloaderfinal/ILogger.java
@@ -30,61 +30,81 @@ import cn.finalteam.toolsfinal.logger.Settings;
 class ILogger {
     public static final String DEFAULT_TAG = "FileDownloaderFinal";
     public static boolean DEBUG = BuildConfig.DEBUG;
-    private static final LoggerPrinter printer = LoggerFactory.getFactory(DEFAULT_TAG, DEBUG);
+    private static LoggerPrinter printer;
 
     //no instance
     private ILogger() {
+        printer = LoggerFactory.getFactory(DEFAULT_TAG, DEBUG);
+    }
+
+    private static void createInstance(){
+        if (printer == null){
+            new ILogger();
+        }
     }
 
     public static void clear() {
+        createInstance();
         printer.clear();
     }
 
     public static Settings getSettings() {
+        createInstance();
         return printer.getSettings();
     }
 
     public static Printer t(String tag) {
+        createInstance();
         return printer.t(tag, printer.getSettings().getMethodCount());
     }
 
     public static Printer t(int methodCount) {
+        createInstance();
         return printer.t(null, methodCount);
     }
 
     public static Printer t(String tag, int methodCount) {
+        createInstance();
         return printer.t(tag, methodCount);
     }
 
     public static void d(String message, Object... args) {
+        createInstance();
         printer.d(message, args);
     }
 
     public static void e(Throwable throwable) {
+        createInstance();
         printer.e(throwable);
     }
 
     public static void e(String message, Object... args) {
+        createInstance();
         printer.e(null, message, args);
     }
 
     public static void e(Throwable throwable, String message, Object... args) {
+        createInstance();
         printer.e(throwable, message, args);
     }
 
     public static void i(String message, Object... args) {
+        createInstance();
         printer.i(message, args);
     }
 
     public static void v(String message, Object... args) {
+        createInstance();
         printer.v(message, args);
     }
 
     public static void w(String message, Object... args) {
+        createInstance();
         printer.w(message, args);
     }
 
     public static void wtf(String message, Object... args) {
+        createInstance();
         printer.wtf(message, args);
     }
 
@@ -94,6 +114,7 @@ class ILogger {
      * @param json the json content
      */
     public static void json(String json) {
+        createInstance();
         printer.json(json);
     }
 
@@ -103,6 +124,7 @@ class ILogger {
      * @param xml the xml content
      */
     public static void xml(String xml) {
+        createInstance();
         printer.xml(xml);
     }
 }
